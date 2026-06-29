939f61d948b36bce5b0ed68211bccbb56adbacdf
natalie-17 <natalieliebl@outlook.de>
2026-06-29 19:41:28 +0200

    pocketbase integration

--- src/components/polaroid.tsx
+++ src/components/polaroid.tsx
@@ -1,7 +1,20 @@
-import type { Component } from 'solid-js';
+import { createEffect, type Component } from 'solid-js';
+import PocketBase from 'pocketbase';
 
-const Polaroid: Component = () => {
+const pb = new PocketBase('https://api.pensionliebl.mkarl.io');
 
+type PolaroidProps = {
+  id: string
+}
+
+const Polaroid: Component<PolaroidProps> = (props: PolaroidProps) => {
+  createEffect(() => {
+    (async () => {
+      const record = await pb.collection('polaroid').getOne(props.id);
+      console.log(record);
+    })();
+  });
+  
   return (
     <>
         <h1>ueberschrift</h1>
--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -5,7 +5,7 @@ const Homepage: Component = () => {
   return (
     <>
       <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
-      <Polaroid/>
+      <Polaroid id='7757ziztm6y4rwu'/>
     </>
   );
 };
